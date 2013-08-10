3841391810a30b848e2cd3c790251eaf2bbc828e
Alex Silvestrov <cohimame@gmail.com>
2013-08-10 17:46:59 +0400

    errors fixed

--- src/main/scala/functionalds.scala
+++ src/main/scala/functionalds.scala
@@ -10,44 +10,33 @@ object LList {
     else 
       Cons(as.head, apply(as.tail: _*))
 
-  def tail[A](LList: LList[A]):LList[A] = LList match {
-    case NNil => sys.error("tail of empty LList")
-    case elem :: tail => tail
+  def concat[A](l1: LList[A],l2: LList[A]): LList[A] = l1 match {
+    case NNil => l2
+    case Cons(elem, tail) => Cons(elem, concat(tail,l2))
   }
 
-  def dropWhile[A](LList: LList)(f: A => Boolean) = LList match {
-    case NNil => NNil
-    case elem :: tail if f(elem) elem :: dropWhile(tail)(f)
-  }
-  
   def append[A](a1: LList[A], a2: LList[A]): LList[A] = a1 match {
-    case Nil => a2
+    case NNil => a2
     case Cons(h,t) => Cons(h, append(t, a2))
   }
 
   def foldRight[A,B](l: LList[A], z: B)(f: (A, B) => B): B = l match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-    }
+    case NNil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
 
   @annotation.tailrec
   def foldLeft[A,B](l: LList[A], z: B)(f: (B, A) => B): B = l match {
-    case Nil => z
+    case NNil => z
     case Cons(h,t) => foldLeft(t, f(z,h))(f)
-  }  
-
-  def fRight[A,B](l: LList[A], z: B)(f: (A,B) => B): B =
-    foldLeft(reverse(l), z)((b,a) => f(a,b))
+  }
 
   def map[A,B](l: LList[A])(f: A => B): LList[B] = 
-    foldRight(l,NNil:LList[A])( (a,b) => Cons(f(a), b) ) 
+    foldRight(l,NNil:LList[B])( (a,b) => Cons(f(a), b) )
 
+  def flatMap[A,B](l: LList[A])(f: A => LList[B]): LList[B] =
+    foldRight(l,NNil:LList[B])( (a,b) => concat(f(a), b) )  
 
 }
 
 import LList._
-
-
-val example = Cons(1, Cons(2, NNil))
-val example2 = LList(1,2,3)
-val total = sum(example)
